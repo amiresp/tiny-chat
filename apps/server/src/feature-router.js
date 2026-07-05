7dd9f9b8a9774254eabec108127322cad0913fbf
Amir Spalani <abasirat92@gmail.com>
2026-07-05 18:30:18 +0330

    Mount message interaction routes

--- apps/server/src/feature-router.js
+++ apps/server/src/feature-router.js
@@ -2,9 +2,11 @@ import express from 'express';
 import { createChatFeatureRouter } from './feature-chats.js';
 import { createOnlineFeatureRouter } from './feature-online.js';
 import { createAvatarFeatureRouter } from './feature-avatar.js';
+import { createInteractionRouter } from './feature-interactions.js';
 
 export function createFeatureRouter({ io }) {
   const router = express.Router();
+  router.use(createInteractionRouter());
   router.use(createChatFeatureRouter({ io }));
   router.use(createOnlineFeatureRouter());
   router.use(createAvatarFeatureRouter());
