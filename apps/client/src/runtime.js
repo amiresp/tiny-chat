a1ab72e9a9037cde5b63f8c4bbfdeef26297f631
Amir Spalani <abasirat92@gmail.com>
2026-07-05 09:47:23 +0330

    Clear stale service workers in live source mode

--- apps/client/src/runtime.js
+++ apps/client/src/runtime.js
@@ -6,3 +6,15 @@ const isNativeShell = Capacitor.isNativePlatform() || window.location.protocol =
 
 export const apiOrigin = import.meta.env.VITE_API_URL || (isNativeShell ? appOrigin : '');
 export const socketOrigin = import.meta.env.VITE_SOCKET_URL || (isNativeShell ? appOrigin : window.location.origin);
+
+if (import.meta.env.DEV && !isNativeShell && 'serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations()
+    .then((registrations) => Promise.all(registrations.map((registration) => registration.unregister())))
+    .catch(() => {});
+
+  if ('caches' in window) {
+    caches.keys()
+      .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+      .catch(() => {});
+  }
+}
