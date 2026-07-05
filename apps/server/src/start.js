57923c726638f5859519a3bfc92072394c7e1b3e
Amir Spalani <abasirat92@gmail.com>
2026-07-05 18:29:11 +0330

    Capture Socket.IO for feature events

--- apps/server/src/start.js
+++ apps/server/src/start.js
@@ -2,5 +2,6 @@ process.on('unhandledRejection', (error) => {
   console.error('Unhandled promise rejection:', error);
 });
 
+await import('./feature-io.js');
 await import('./feature-mount.js');
 await import('./index.js');
