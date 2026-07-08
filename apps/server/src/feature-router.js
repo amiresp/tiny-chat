f0c7436101ec548c0de72f62899a4ecdafe2674f
Amir Spalani <abasirat92@gmail.com>
2026-07-08 16:29:17 +0330

    Mount robust media gallery route

--- apps/server/src/feature-router.js
+++ apps/server/src/feature-router.js
@@ -6,12 +6,14 @@ import { createInteractionRouter } from './feature-interactions.js';
 import { createMessagePageRouter } from './feature-message-pages.js';
 import { createRssProxyRouter } from './feature-rss-proxy.js';
 import { createFeaturePackRouter } from './feature-pack.js';
+import { createMediaFeatureRouter } from './feature-media.js';
 
 export function createFeatureRouter({ io }) {
   const router = express.Router();
   router.use(createRssProxyRouter());
   router.use(createFeaturePackRouter());
   router.use(createMessagePageRouter());
+  router.use(createMediaFeatureRouter());
   router.use(createInteractionRouter());
   router.use(createChatFeatureRouter({ io }));
   router.use(createOnlineFeatureRouter());
