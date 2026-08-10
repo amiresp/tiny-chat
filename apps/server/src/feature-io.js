da4444f3a7b8bf6762e5c54341e3220161ce46c1
Amir Spalani <abasirat92@gmail.com>
2026-08-10 13:09:50 +0330

    fix: attach feature presence tracker to the live socket server

--- apps/server/src/feature-io.js
+++ apps/server/src/feature-io.js
@@ -1,10 +1,16 @@
 import { Server } from 'socket.io';
+import { attachFeaturePresence } from './feature-state.js';
 
 let ioInstance = null;
+let presenceAttached = false;
 const originalUse = Server.prototype.use;
 
 Server.prototype.use = function captureFeatureIo(...args) {
   ioInstance = this;
+  if (!presenceAttached) {
+    presenceAttached = true;
+    attachFeaturePresence(this);
+  }
   return originalUse.apply(this, args);
 };
 
